ba86ac0228f1a0b869a7b5f1c4cd87652a68a469
anshulv1401 <anshulv1401@gmail.com>
2025-07-20 20:02:54 +0530

    impl for duckdb read added

--- Databases/DuckDbDatabaseAdapter.cs
+++ Databases/DuckDbDatabaseAdapter.cs
@@ -1,16 +1,64 @@
 ﻿using DataQueryAndExportSystem.Models;
+using DuckDB.NET.Data;
 using System.Data;
 using System.Text;
 using static DataQueryAndExportSystem.Enums.DataServiceEnums;
 
 namespace DataQueryAndExportSystem.Databases;
 
-public class DuckDbDatabaseAdapter(ILogger<DuckDbDatabaseAdapter> logger, ApplicationConfiguration configuration) : IDatabaseAdapter
+public class DuckDbDatabaseAdapter(ILogger<DuckDbDatabaseAdapter> logger) : IDatabaseAdapter
 {
     public DatabaseType DatabaseType { get { return DatabaseType.DuckDb; } }
 
-    public override async Task<(List<List<KeyValuePair<string, dynamic?>>>, List<KeyValuePair<string, string>>)> FetchData(ConnectionDetails sqlConnection, string queryString, int? limit = null)
+    public async Task<List<List<KeyValuePair<string, dynamic?>>>> FetchData(ConnectionDetails connectionDetails, string queryString, int? limit = null)
     {
-        throw new NotImplementedException("DuckDB support is not implemented yet.");
+        await using var connection = new DuckDBConnection(GetConnectionString(connectionDetails));
+        try
+        {
+            logger.LogInformation("Opening Connection");
+            await connection.OpenAsync();
+            logger.LogInformation("Connection Opened");
+
+            using var command = connection.CreateCommand();
+            command.CommandText = queryString;
+
+            var results = new List<List<KeyValuePair<string, dynamic?>>>();
+            var dataTypes = new List<KeyValuePair<string, string>>();
+
+            using var reader = await command.ExecuteReaderAsync();
+            logger.LogInformation("Executing query {@query}", queryString);
+            while (await reader.ReadAsync())
+            {
+                var row = new List<KeyValuePair<string, dynamic?>>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row.Add(new KeyValuePair<string, dynamic?>(reader.GetName(i), reader.IsDBNull(i) ? null : reader.GetValue(i)));
+                }
+                results.Add(row);
+
+                if (limit.HasValue && limit-- <= 0)
+                    break;
+            }
+
+            return (results);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Error occurred: {@Error}", ex.Message);
+            throw;
+        }
+        finally
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+                logger.LogInformation("Connection Closed\n");
+            }
+        }
+    }
+
+    public string GetConnectionString(ConnectionDetails connection)
+    {
+        return $"DataSource={connection.Server}AccessMode=ReadWriteCreate;";
     }
 }
--- Databases/IDatabaseAdapter.cs
+++ Databases/IDatabaseAdapter.cs
@@ -3,6 +3,8 @@ namespace DataQueryAndExportSystem.Databases
 {
     public interface IDatabaseAdapter
     {
-        Task<(List<List<KeyValuePair<string, dynamic?>>>, List<KeyValuePair<string, string>>)> FetchData(ConnectionDetails sqlConnection, string queryString, int? limit = null);
+        Task<List<List<KeyValuePair<string, dynamic?>>>> FetchData(ConnectionDetails sqlConnection, string queryString, int? limit = null);
+
+        public string GetConnectionString(ConnectionDetails connection);
     }
 }
